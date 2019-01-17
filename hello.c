69d9ee8d2c6843a8e881275e6fdbfce732f9e217
Max Van Arsdall <maxvanarsdall@huskers.unl.edu>
2019-01-17 12:57:24 -0600

    Create hello.c
    Changed name and added line to put major

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Chris Bourke
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Max Van Arsdall\n");
+  printf("Undecided\n")
+  return 0;
+}
